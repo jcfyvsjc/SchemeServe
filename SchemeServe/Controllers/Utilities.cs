1a5be42ae14cfca3375fa3eca587204e17b83202
jcfyvsjc <142131870+jcfyvsjc@users.noreply.github.com>
2023-08-15 11:12:28 +0700

    Add files via upload

---
+++ SchemeServe/Controllers/Utilities.cs
@@ -0,0 +1,30 @@
+﻿using System.Globalization;
+
+namespace SchemeServe;
+
+public static class Utilities
+{
+    public static DateTime StringToDate(string dateAsString)
+    {
+        DateTime dateTime;
+        if (DateTime.TryParseExact(dateAsString, "yyyy-MM-dd", CultureInfo.InvariantCulture, DateTimeStyles.None,
+                out dateTime))
+
+        {
+            return dateTime;
+        }
+
+        else
+        {
+            throw new ArgumentException("Invalid date format.");
+        }
+    }
+
+    public static string DateToString(DateTime dateTime)
+    {
+        string dateAsString = dateTime.ToString("yyyy-MM-dd");
+        return dateAsString;
+    }
+
+}
+
